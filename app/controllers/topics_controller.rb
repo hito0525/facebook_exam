d2f5288ad7ba980aa9d9c3172580a5cb3906a3a2
Takahito Iwase <hito.0525@gmail.com>
2017-04-16 20:30:46 +0900

    add some details of edit update destroy actions

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,19 +1,37 @@
 class TopicsController < ApplicationController
   def index
     @topics = Topic.all
+    @topics = Topic.order(:created_at).reverse_order
+
   end
 
   def new
-     @topic = Topic.new
+    @topic = Topic.new
   end
 
   def create
-      Topic.create(params[:Topic])
+    Topic.create(topics_params)
+    redirect_to topics_path, notice: "トピックスを作成しました！"
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic =Topic.find(params[:id])
+    @topic.update(topics_params)
+    redirect_to topics_path
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+    @topic.destroy
+    redirect_to topics_path,notice: "ブログを削除しました！"
   end
 
   private
     def topics_params
       params.require(:topic).permit(:title, :content)
     end
-
 end
