0ff6193759fd9324513e4f461b50373a332f4909
Takahito Iwase <hito.0525@gmail.com>
2017-06-15 16:54:25 +0900

    making association between user and topic model

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,6 @@
 class Topic < ActiveRecord::Base
   validates :title, :content, presence: true
   belongs_to :user
+  has_many :comments, dependent: :destroy
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ActiveRecord::Base
 
   mount_uploader :avatar, AvatarUploader
 
-  has_many :topics
+  has_many :topics, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(provider: auth.provider, uid: auth.uid).first
