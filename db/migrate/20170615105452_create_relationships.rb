94a0b0e1f9def64d5064fa51debc470f4de99583
Takahito Iwase <hito.0525@gmail.com>
2017-06-16 11:08:51 +0900

    migraton and add index as unique between follower_id adn followed_id

--- db/migrate/20170615105452_create_relationships.rb
+++ db/migrate/20170615105452_create_relationships.rb
@@ -8,5 +8,6 @@ class CreateRelationships < ActiveRecord::Migration
     end
     add_index :relationships, :follower_id
     add_index :relationships, :followed_id
+    add_index :relationships, [:follower_id, :followed_id], unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614095710) do
+ActiveRecord::Schema.define(version: 20170615105452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 20170614095710) do
 
   add_index "comments", ["topic_id"], name: "index_comments_on_topic_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true, using: :btree
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
