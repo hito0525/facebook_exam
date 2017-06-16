82afcb0b6720840a554b35aaa59379beb367b8f3
Takahito Iwase <hito.0525@gmail.com>
2017-06-16 11:33:05 +0900

    generating users controller and user's view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   devise_for :users, controllers: {
@@ -11,4 +12,5 @@ Rails.application.routes.draw do
     post :confirm, on: :collection
     end
     root 'top#index'
+  resources :users, only: [:index]
 end
