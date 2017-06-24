9b5c7a13f0e6f92d062b2d9577070983a1c114f5
Takahito Iwase <hito.0525@gmail.com>
2017-06-24 17:57:37 +0900

    creating Messages Controller with index and create action,creating conversation model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616031557) do
+ActiveRecord::Schema.define(version: 20170624085037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20170616031557) do
 
   add_index "comments", ["topic_id"], name: "index_comments_on_topic_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
